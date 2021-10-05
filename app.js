6fbd592bb9b6cc88153bfb5b4c074217cbf19af2
aakankshabaijal <f20190304@goa.bits-pilani.ac.in>
2021-10-05 23:44:59 +0530

    Fixed delete and decimal operations

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-const outputArea = document.querySelector('.output');
-
 const resultDisplay = document.querySelector('.current-operand');
 const previousNumber = document.querySelector('.previous-operand');
 
 let num1 = 0;
 let num2 = 0;
-let currOperator ='';
-
+let currOperator =undefined;
 
 const numbers = document.querySelectorAll('.number');
 
@@ -22,87 +19,124 @@ const delButton = document.querySelector('.del');
 
 for(let number of numbers){
     number.addEventListener('click', () => {
-        let currDigit = number.innerText;
-        console.log(currDigit); 
-        resultDisplay.innerText += currDigit;
-    })
-}
-
-/**
- * When an operator (add,subtact, multiply or divide is pressed)
- */
+        let currDigit = number.innerText; //this gives us the current digit in string format
+        //we can allow only one decimal point in a number
+        if(currDigit === '.' && resultDisplay.innerText.includes('.')){
+            return;
+        }
+        resultDisplay.innerText += currDigit; //this allows us to append the current digit        
+    });
+};
 
 const operatorPressed = () => {
     num1 = resultDisplay.innerText ;
-    console.log(num1);
     previousNumber.innerText = num1;
+
+    switch(currOperator){
+        case 'add':
+            previousNumber.innerText+=' +';
+            break;
+        case 'subtract':
+            previousNumber.innerText+=' -';
+            break;
+        case 'multiply':
+            previousNumber.innerText+=' ×';
+            break;
+        case 'divide':
+            previousNumber.innerText+=' ÷';
+            break;
+        default:
+            return;
+    }
     resultDisplay.innerText = '';
 }
 
 
 addOperator.addEventListener('click', () => {
-    operatorPressed();
+    if(resultDisplay.innerText === '')
+        return;
     currOperator = 'add';
+    operatorPressed();
 });
 
 subtractOperator.addEventListener('click', () => {
-    operatorPressed();
+    if(resultDisplay.innerText === '')
+        return;
     currOperator = 'subtract';
+    operatorPressed();
 });
 
 multiplyOperator.addEventListener('click', () => {
-    operatorPressed();
+    if(resultDisplay.innerText === '')
+        return;
     currOperator = 'multiply';
+    operatorPressed();
 });
 
 divideOperator.addEventListener('click', () => {
-    operatorPressed();
+    if(resultDisplay.innerText === '')
+        return;
     currOperator = 'divide';
+    operatorPressed();
 });
 
 equalToButton.addEventListener('click', () => {
+    if(resultDisplay.innerText === '')
+        return;
     num2 = resultDisplay.innerText;
-    console.log(num2);
+    let result = 0;
 
     switch(currOperator){
         case 'add' :
-            resultDisplay.innerText = parseInt(num1)+ parseInt(num2);
+            result = eval(num1 + '+' + num2);
+            previousNumber.innerText = `${num1} + ${num2} = `;            
             break;
+
         case 'subtract' :
-            resultDisplay.innerText = parseInt(num1) -  parseInt(num2);
+            result = eval(num1 + '-' + num2);
+            previousNumber.innerText = `${num1} - ${num2} = `;      
             break;
+
         case 'multiply' :
-            resultDisplay.innerText = parseInt(num1) * parseInt(num2);
+            result = eval(num1 + '*' + num2);
+            previousNumber.innerText = `${num1} × ${num2} = `;
             break;
+
         case 'divide' :
-            resultDisplay.innerText = parseInt(num1) / parseInt(num2);
+            result = eval(num1 + '/' + num2);
+            previousNumber.innerText = `${num1} ÷ ${num2} = `;
             break;
+
+        default:
+            return;
     }
 
-})
+    if(result.toString().includes('.'))
+    {
+        let index = result.toString().indexOf('.');
+        if(result.toString().length - index > 5)
+            result = result.toFixed(2); //to handle 0.1 + 0.2 = 0.300000004 in JS
+    }
+        
+    resultDisplay.innerText = result;
+});
 
 allClearButton.addEventListener('click', () => {
     num1 = 0;
     num2 = 0;
     previousNumber.innerText='';
     resultDisplay.innerText='';
-})
+    currOperator = undefined;
+});
 
 delButton.addEventListener('click', () => {
-    if(num1 !== 0){
-        num1 = num1/10;
-        resultDisplay.innerText = parseInt(num1);
-    }
-})
+    let curr = resultDisplay.innerText;
+    curr = curr.slice(0,curr.length-1);    
+    resultDisplay.innerText = curr;    
+});
 
 /**
- * !Problems :
- * 1. Delete Button not working in some cases
- * 2. Decimal operations not working
- * 
- * ?Additional Features: Comma Separator
- * 
- * Continue watching from 17:58 on https://youtu.be/j59qQ7YWLxw?t=1067
- * 
+ * !Decimal operations not working (0.1 + 0.2 = 0.30000000004) according to JS * 
+ * ?Additional Features: Comma Separator *  
  */
 
